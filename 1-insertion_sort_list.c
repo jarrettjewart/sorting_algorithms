731256e12725c2b5b74458c160c00e4cc1cbf03a
jarrettjewart <jarrett_jewart@yahoo.com>
2022-04-19 19:43:27 -0700

    Big0Q1 redone

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,11 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts list in order
- * @list: ptr to dble list
+ * insertion_sort_list - sorts a doiubly linked list in ascending order
+ * @list: pointer to doubly linked list
+ *
+ * Return: void
  */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *tmp;
@@ -30,24 +31,24 @@ void insertion_sort_list(listint_t **list)
 }
 
 /**
- * swapNode - swaps nodes based on comparison
- * @list: list of nodes
- * @node1: first node
- * @node2: second ndode
+ * swapNode - swaps two adjacent nodes based on comparison
+ * @list: the list of nodes
+ * @node1: the first node in sequence
+ * @node2: the second node in sequence
+ * Return: void
  */
-
 void swapNode(listint_t **list, listint_t *node1, listint_t *node2)
 {
 	if (node1->prev)
 		node1->prev->next = node2;
-if (node2->next)
-	node2->next->prev = node1;
-
-node1->next = node2->next;
-node2->prev = node1->prev;
-node2->next = node1;
-node1->prev = node2;
-while ((*list)->prev)
-	*list = (*list)->prev;
-print_list(*list);
+	if (node2->next)
+		node2->next->prev = node1;
+	/* swaps node pointers, thus moving them */
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node2->next = node1;
+	node1->prev = node2;
+	while ((*list)->prev)
+		*list = (*list)->prev;
+	print_list(*list);
 }
